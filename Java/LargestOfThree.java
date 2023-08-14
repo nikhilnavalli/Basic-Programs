68463d54632c5015d1fe07f9f39af6a8a1b628a7
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-08-14 22:39:35 +0530

    Largest among three numbers program added.
    This program takes three numbers as input from the user and find the largest of those three numbers and prints the result.

---
+++ Java/LargestOfThree.java
@@ -0,0 +1,32 @@
+// Java program to find the largest of three numbers.
+
+
+import java.util.Scanner;
+
+class LargestOfThree{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        String x = sc.next();
+        int a = Integer.parseInt(x);
+        System.out.print("Enter second number : ");
+        String y = sc.next();
+        int b = Integer.parseInt(y);
+        System.out.print("Enter third number : ");
+        String z = sc.next();
+        int c = Integer.parseInt(z);
+
+        if( a == b && b == c){
+            System.out.println("All numbers are equal");
+        }
+        else if(a >= b && a >= c)
+            System.out.println(a + " is the largest number.");
+
+        else if (b >= a && b >= c)
+            System.out.println(b + " is the largest number.");
+
+        else
+            System.out.println(c + " is the largest number.");
+    }
+}
