203c25f6b2c3afd61c14696c2d3efe978c4083cf
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-08-23 21:48:47 +0530

    Update positive_negative.py

--- Python/positive_negative.py
+++ Python/positive_negative.py
@@ -8,3 +8,6 @@ elif num == 0:
    print('Entered number is zero.')
 else:
    print('Entered number is a negative number.')
+
+# Printing the result using f string or string literals
+# print(f'Entered number is a postive number') or print(f'{num} is a positive number')
