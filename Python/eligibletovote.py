c043cdb77696e4931f57ea902ca079a7a12d240a
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-08-25 22:29:26 +0530

    Update eligibletovote.py

--- Python/eligibletovote.py
+++ Python/eligibletovote.py
@@ -4,7 +4,12 @@ age = int(input('Enter your age : '))
 
 if(age <= 18):
     print("Sorry! You are not eligible for voting")
+    # print(f'Your age is {age} and you are not eligible for voting')
 elif(age > 18 and age < 120):
     print('You are eligible for voting')
+    # print(f'Your age is {age} and you are eligible for voting')
 elif(age >= 120):
     print('Age is invalid. Please enter a valid age')
+    # print(f'You entered {age} age and it is an invalid age')
+
+# Another way of printing the result using the string literals or the f string
