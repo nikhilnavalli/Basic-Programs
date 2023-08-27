3412fbb43fb4a38d97165d59eaf69800fa28570a
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-08-27 21:22:44 +0530

    Update areaofcircle.py

--- Python/areaofcircle.py
+++ Python/areaofcircle.py
@@ -5,6 +5,7 @@ r = float(input('Enter radius of the circle : '))
 area = 3.142 * r * r
 
 print('Area of the circle with the radius '+str(r)+' is %0.2f' %area)
+#print(f'Area of the circle with the radius {r} is {area}')
 
 # Printing the result using string literals or f string
 # print(f'Area of the circle with the radius {r} is {area}')
