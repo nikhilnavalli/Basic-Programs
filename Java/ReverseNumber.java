43956e565c0c74a98b280a88769e788061d3eb0c
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-09-01 23:04:30 +0530

    Update ReverseNumber.java

--- Java/ReverseNumber.java
+++ Java/ReverseNumber.java
@@ -13,17 +13,11 @@ class ReverseNumber{
     
     System.out.println("Original Number: " + num);
 
-    // run loop until num becomes 0
     while(num != 0) {
-    
-      // get last digit from num
       int digit = num % 10;
       reversed = reversed * 10 + digit;
-
-      // remove the last digit from num
       num /= 10;
     }
-
     System.out.println("Reversed Number: " + reversed);
   }
 }
