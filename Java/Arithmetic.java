0f4ed6e0e74294c135f7ba31d5eb1c2daee7e042
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-09-02 23:56:27 +0530

    Update Arithmetic.java

--- Java/Arithmetic.java
+++ Java/Arithmetic.java
@@ -9,15 +9,16 @@ class Arithmetic{
         System.out.print("Enter first number : ");
         String x = sc.next();
         int a = Integer.parseInt(x);
+        
         System.out.print("Enter second number : ");
         String y = sc.next();
         int b = Integer.parseInt(y);
 
-        System.out.println("-------------------------------------------");
+        System.out.println("*********************************************");
         System.out.println("The addition of "+a+" and "+b+" is : "+ (a+b));
         System.out.println("The subtraction of "+a+" and "+b+" is : "+ (a-b));
         System.out.println("The multiplication of "+a+" and "+b+" is :"+ (a*b));
         System.out.println("The division of "+a+" and "+b+" is : "+ (a/b));
-        System.out.println("-------------------------------------------");
+        System.out.println("*********************************************");
     }
 }
