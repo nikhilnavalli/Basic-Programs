85a0d0061f7209ac5368ffbb91d457d5db4f69f0
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-09-10 23:35:04 +0530

    Subtraction of two numbers program added
    This program takes two numbers as input from the user, calculates the difference of those two numbers and prints the result.

---
+++ C/subtraction_of_two_numbers.c
@@ -0,0 +1,18 @@
+// C program to calculate subtraction of two integers.
+
+#include <stdio.h>
+int main() {    
+
+    int a, b, sum;
+    
+    printf("Enter the fist number : ");
+    scanf("%d", &a);
+
+    printf("Enter the second number : ");
+    scanf("%d", &b);
+
+    sub = a - b;      
+    
+    printf("Sum of %d and %d is = %d", a, b, sub);
+    return 0;
+}
