ae360237c348ef8884cc5179da709a7a39b472fc
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-09-12 01:00:31 +0530

    Multiplication of two numbers program added
    This program takes two numbers as input from the user, calculates the product of those two numbers and then prints the result.

---
+++ C/multiplication_of_two_numbers.c
@@ -0,0 +1,18 @@
+// C program to calculate product of two integers.
+
+#include <stdio.h>
+int main() {    
+
+    int m, n, product;
+    
+    printf("Enter the fist number : ");
+    scanf("%d", &m);
+
+    printf("Enter the second number : ");
+    scanf("%d", &n);
+
+    product = a * b;      
+    
+    printf("Product of %d and %d is = %d", a, b, sub);
+    return 0;
+}
