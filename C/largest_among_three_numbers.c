fd673bbc584c999fd03044e165e4f2ded41db9b2
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-09-22 22:30:39 +0530

    Largest of three numbers program added
    This program takes three numbers as input from the user, finds the largest number among those three numbers and prints the result.

---
+++ C/largest_among_three_numbers.c
@@ -0,0 +1,26 @@
+// C program to find largest number among three numbers.
+
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    
+    printf("Enter first number : ");
+    scanf("%d", &a);
+
+    printf("Enter second number : ");
+    scanf("%d", &b);
+ 
+    printf("Enter third number : ");
+    scanf("%d", &c);
+
+    if(a > b && a > c){
+        printf("%d, first number is the largest", a);
+    }
+    else if(b > a && b > c){
+        printf("%d, second number is the is largest", b);
+    }
+    else{
+        printf("%d, third number is the is largest", c);
+    }
+    return 0;
+}
