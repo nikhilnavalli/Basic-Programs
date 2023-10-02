fee0223bda46199895b94fb7b3489d883a20c714
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-02 23:38:36 +0530

    Update swap_two_numbers_2.c

--- C/swap_two_numbers_2.c
+++ C/swap_two_numbers_2.c
@@ -12,9 +12,12 @@ int main() {
 
   printf("\nBefore swapping, first number = %.2f\n", a);
   printf("Before swapping, second number = %.2f", b);
-  
+
+  // Add a and b values and assign it to b. The new b will become sum of a and b.
   b = a + b;
+  // Now subtract a value from new b value and assign it to a. The new a will become difference of new b and a.
   a = b - a;
+  // At last subtract new a from new and assign it to b.
   b = b - a;
 
   printf("\nAfter swapping, first number = %.2f\n", a);
@@ -22,3 +25,5 @@ int main() {
 
   return 0;
 }
+
+// For better understanding assume a = 10 and b = 20 or any other values of your choice and follow the 17th , 19th and 21st lines of code.
