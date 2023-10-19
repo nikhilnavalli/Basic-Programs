7a9d4d8477c6718b7c4af51e3b454d2784ebfefe
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-19 22:43:39 +0530

    Update factorial.c

--- C/factorial.c
+++ C/factorial.c
@@ -5,12 +5,21 @@
 int main(){
     int n, i, fact = 1;
 
-    printf("Enter the number whose factorial you want : ");
+    printf("Enter a number : ");
     scanf("%d", &n);
 
-    for(i = 1; i <= n; i++){
-        fact = fact * i;
+    if(n == 0 || n == 1){
+        printf("Factorial of %d is 1", n);
+    }
+    else if(n < 0){
+        printf("You entered a negative number, enter a positive number");
+    }
+    else{
+        for(i = 1; i <= n; i++){
+            fact = fact * i;
+        }
+    printf("Factorial of %d is %d", n, fact);
     }
 
-    printf("Factorial of %d is %d", n, fact);
+    return 0;
 }
