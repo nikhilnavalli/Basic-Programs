6d459b9a8198ed55872cb4a9d2eae8c9488ff73a
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-21 22:19:36 +0530

    Create GCD.c
    This program takes two numbers as input from the user, calculates the GCD of those two numbers and prints the result.

---
+++ C/GCD.c
@@ -0,0 +1,21 @@
+// C program to find GCD of two numbers.
+
+#include <stdio.h>
+int main()
+{
+    int a, b, i, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    for(i=1; i <= a && i <= b; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(a%i==0 && b%i==0)
+            gcd = i;
+    }
+
+    printf("G.C.D of %d and %d is %d", a, b, gcd);
+
+    return 0;
+}
