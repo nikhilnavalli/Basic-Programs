38e293e52f0751975f50988816b662bc1464199c
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-27 22:58:38 +0530

    Update swap_two_numbers.py

--- Python/swap_two_numbers.py
+++ Python/swap_two_numbers.py
@@ -3,8 +3,12 @@
 a=int(input("Enter first number : "))
 b=int(input("Enter second number : "))
 
-print("Before swapping values : a = "+str(a)+" and b = "+str(b) )
+print("Before swapping values : a = "+str(a)+" and b = "+str(b))
 a=a+b
 b=a-b
 a=a-b
-print("After swapping values : a = "+str(a)+" and b = "+str(b) )
+print("After swapping values : a = "+str(a)+" and b = "+str(b))
+
+# We can also print using f string or string literals in line number 6 and 10 respectively as given below
+# print(f"Before swapping values : a = {a} and b = {b}")
+# print(f"After swapping values : a = {a} and b = {b}")
