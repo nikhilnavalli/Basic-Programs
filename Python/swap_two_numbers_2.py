2ec10555c61bf61088c9af8488f87b93004d5bf2
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-28 23:05:36 +0530

    Update swap_two_numbers_2.py

--- Python/swap_two_numbers_2.py
+++ Python/swap_two_numbers_2.py
@@ -7,3 +7,7 @@ temp = a
 a = b
 b = temp
 print("After swapping the values a = "+str(a)+" b = "+str(b))
+
+# We can also print the results using the f string or string literals in the line number 5 and 9 respectively as given below.
+# print(f"Before swapping the values a = {a} and b = {b}")
+# print(f"After swapping the values a = {a} and b = {b}")
