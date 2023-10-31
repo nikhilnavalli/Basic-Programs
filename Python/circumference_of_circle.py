09336e824e9b909ecc6c30ed737d4ecf889077ff
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-31 23:04:41 +0530

    Update circumference_of_circle.py

--- Python/circumference_of_circle.py
+++ Python/circumference_of_circle.py
@@ -5,3 +5,6 @@ PI = 3.14159265359
 circumference = 2 * PI * radius
 
 print("The circumference of circle with radius "+str(radius)+" is = "+str(circumference))
+
+# We can also print the result using the f string or string literals as given below.
+# print(f"The circumference of circle with radius {radius} is {circumference}")
