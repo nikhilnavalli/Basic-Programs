fa354dc2fb424dc3d43ae305d702be2d9fd10252
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-10-31 23:02:57 +0530

    Create circumference_of_circle.py
    This program takes the radius of a circle as input from the user, calculates the circumference of that circle and prints the result.

---
+++ Python/circumference_of_circle.py
@@ -0,0 +1,7 @@
+# Python program to calculate the circumference of a circle with the given radius.
+
+radius = float(input("Enter the radius of circle : "))
+PI = 3.14159265359
+circumference = 2 * PI * radius
+
+print("The circumference of circle with radius "+str(radius)+" is = "+str(circumference))
