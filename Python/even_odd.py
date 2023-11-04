cdc17834a3a7181e99e439e73497c64ae0d9173e
Nikhil S Navalli <115411882+nikhilnavalli@users.noreply.github.com>
2023-11-04 23:00:54 +0530

    Create even_odd.py
    This program takes a number as input from the user, checks whether the entered number is odd or even and prints the result.

---
+++ Python/even_odd.py
@@ -0,0 +1,8 @@
+# Python program to check whether the entered number is odd or even.
+
+number = int(input("Enter a number : "))
+
+if(number%2 == 0):
+  print(str(number)+" is an even number")
+else:
+  print(str(number)+" is an odd number")
